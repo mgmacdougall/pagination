26d38cd699fecb52dedcdd284cdf9b31544545aa
mgmacdougall <MacD>
2020-07-20 15:15:56 -0400

    Add of comments and rearrange of code.

--- js/script.js
+++ js/script.js
@@ -3,24 +3,43 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-// Global Variables:
+/*** 
+Global variables
+***/
 let studentWindow = window;
 let totalStudents = document.getElementsByClassName('student-item');
 
-// Builds the student list
+/***
+ * Builds the student list for the page limit passed in
+ * defaulting to the first page if none other supplied
+ */
 const buildStudentLists = (list, page = 1) => {
-	let start = page * 10 - 10;
-	let finish = page * 10;
+	let startPageIndex = page * 10 - 10;
+	let finishPageIndex = page * 10;
+
 	for (let i = 0; i < list.length; i++) {
-		if (!(i >= start && i < finish)) {
+		if (!(i >= startPageIndex && i < finishPageIndex)) {
 			list[i].style.display = 'none';
 		}
 	}
 };
 
-// Builds the button strip
-const buildButtonStrip = (list) => {
+/***
+ * Create the given button link item
+ */
+const paginationButton = (buttonNum) => {
+	let li = document.createElement('li');
+	let link = document.createElement('a');
+	link.setAttribute('href', '#');
+	link.textContent = `${buttonNum}`;
+	li.appendChild(link);
+	return li;
+};
+
+/***
+ * Create the button strip for navigation
+ */
+const appendPageLinks = (list) => {
 	let totalButtons = Math.ceil(list.length / 10); // total number of buttons
 
 	let pageContainer = document.getElementsByClassName('page')[0]; // Get the 'page' class'
@@ -29,19 +48,8 @@ const buildButtonStrip = (list) => {
 	let buttonListItemContainer = document.createElement('ul');
 	buttonListItemContainer.className = 'pagination';
 
-	// Create the buttons list inner function for now
-	const getli = (buttonNum) => {
-		let li = document.createElement('li');
-		let link = document.createElement('a');
-		link.setAttribute('href', '#');
-		link.textContent = `${buttonNum}`;
-		li.appendChild(link);
-		return li;
-	};
-
-	// Now loop through the totalButtons
-	for (let i = 0; i < totalButtons; i++) {
-		let res = getli(i + 1);
+	for (let buttonIndex = 0; buttonIndex < totalButtons; buttonIndex++) {
+		let res = paginationButton(buttonIndex + 1);
 		buttonListItemContainer.appendChild(res);
 	}
 
@@ -49,19 +57,10 @@ const buildButtonStrip = (list) => {
 	pageContainer.appendChild(buttonContainer);
 };
 
-// Sets the list to the first 10 items when page is reloaded
-studentWindow.addEventListener('DOMContentLoaded', () => {
-	buildStudentLists(totalStudents);
-	buildButtonStrip(totalStudents);
-	setActiveClassOnButton();
-});
-
-const resetStudentsList = (list) => {
-	for (let i = 0; i < list.length; i++) {
-		list[i].style.display = '';
-	}
-};
-
+/***
+ * Sets the 'active' class to the apprporiate button in the button strip
+ * removing the 'active' class from any previous buttons clicked
+ */
 const setActiveClassOnButton = (buttonIndex = 1) => {
 	let pageButtons = document.getElementsByTagName('a');
 	let activeButton = pageButtons[buttonIndex - 1];
@@ -75,44 +74,44 @@ const setActiveClassOnButton = (buttonIndex = 1) => {
 	}
 };
 
+/***
+ * Resets the display property for each of the students in the list
+ */
+const resetStudentListToVisible = (list) => {
+	for (let i = 0; i < list.length; i++) {
+		list[i].style.display = '';
+	}
+};
+
+/***
+ * Main show page functionality, that builds the student list
+ */
+const showPage = (pageNo) => {
+	buildStudentLists(totalStudents);
+	appendPageLinks(totalStudents);
+	setActiveClassOnButton(pageNo);
+};
+
+/***
+ * Controls the click event on the bubbled up 'a' event
+ * Filters and displays the appropriate students
+ * Sets the Active on the button based on the button clicked
+ */
 studentWindow.addEventListener('click', (event) => {
 	if (event.target.tagName === 'A') {
 		let pageNumber = event.target.innerText;
-		setActiveClassOnButton(pageNumber);
-		resetStudentsList(totalStudents);
+		resetStudentListToVisible(totalStudents);
 		buildStudentLists(totalStudents, pageNumber);
+		setActiveClassOnButton(pageNumber);
 	}
 });
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+/**
+ * DOM Content Loaded Listener that will create the page, button strip, and buttons
+ * sets the default
+ * By design the page will load the first 10 students
+ */
+studentWindow.addEventListener('DOMContentLoaded', () => {
+	const defaultActiveButtonIndex = 1;
+	showPage(defaultActiveButtonIndex);
+});
